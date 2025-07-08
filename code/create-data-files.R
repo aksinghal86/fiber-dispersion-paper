c9c111d6c393073e4ea520b9db4151754b162495
aksinghal86 <aksinghal.aks@gmail.com>
2025-07-08 12:09:28 -0700

    updated scripts

--- code/create-data-files.R
+++ code/create-data-files.R
@@ -9,16 +9,7 @@ polar_coords <- function() {
     mutate(radians = 2*pi*degrees/360) 
 }
 
-debris <- excel_sheets('data/Fiber Dispersion Compilation_Debris.xlsx') %>% 
-  map_dfr(~ read_xlsx('data/Fiber Dispersion Compilation_Debris.xlsx', sheet = .x, col_types = 'text') %>% 
-            mutate(id = .x)) %>% 
-  janitor::clean_names() %>% 
-  select(id, field, fiber_no = fiber, debris) %>% 
-  separate(id, c("rjlg_sample_id", 'chem_risk_id'), sep = ' ') %>% 
-  mutate(debris = debris == 'yes') %>% 
-  mutate_at(vars(rjlg_sample_id, field, fiber_no), as.numeric) %>% 
-  fill(field, .direction = 'down') 
-write_rds(debris, 'data/processed/debris-data.rds')
+
 
 fiber_data <- read_csv(here::here('data/Trial 1 All Fiber Data.csv'), col_types = cols(.default = 'c')) %>% 
   janitor::clean_names() %>% 
@@ -31,11 +22,28 @@ fiber_data <- read_csv(here::here('data/Trial 1 All Fiber Data.csv'), col_types 
          aed_henn = sqrt(fiber_density*log(2*aspect_ratio))*width, # Henn (1996) 
          aed = 66*width*(aspect_ratio/(2 + 4*aspect_ratio))^2.2, # Timbrell (1965)
          aed_hinds = width * fiber_density^0.5
-         ) %>%
+  ) %>%
   filter(!is.na(length)) %>% 
-  left_join(polar_coords(), by = 'direction') %>% 
-  left_join(debris, by = c('rjlg_sample_id', "chem_risk_id", "field", "fiber_no")) 
+  left_join(polar_coords(), by = 'direction') 
 write_rds(fiber_data, 'data/processed/fiber-data.rds')
+
+debris <- excel_sheets('data/TOTAL Fiber Dispersion Compilation_Debris.xlsx') %>% 
+  map_dfr(~ read_xlsx('data/TOTAL Fiber Dispersion Compilation_Debris.xlsx', sheet = .x, col_types = 'text') %>% 
+            mutate(id = .x)) %>% 
+  janitor::clean_names() %>% 
+  select(id, fiber_no = fiber, fiber_type, length = length_um, width = width_um, debris) %>% 
+  separate(id, c("rjlg_sample_id", 'chem_risk_id'), sep = ' ') %>% 
+  mutate_at(vars(rjlg_sample_id, fiber_no, length, width), as.numeric) %>% 
+  mutate(debris = debris == 'yes', 
+         aspect_ratio = length/width,
+         fiber_type = ifelse(fiber_type == 'A', 'Crocidolite', 'Chrysotile')) %>% 
+  filter(!is.na(debris)) |>
+  # group_by(chem_risk_id) |> 
+  # fill(field, .direction = 'down') |> 
+  left_join(fiber_data |> select(rjlg_sample_id, chem_risk_id, distance_ft, direction, orientation) |> distinct(), 
+            by = c('rjlg_sample_id', 'chem_risk_id')) |> 
+  left_join(polar_coords(), by = 'direction') 
+write_rds(debris, 'data/processed/debris-data.rds')
 
 concentrations <- read_xlsx('data/concentration-data.xlsx') %>% 
   janitor::clean_names() %>% 
